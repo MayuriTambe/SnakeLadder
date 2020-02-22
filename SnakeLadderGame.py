6bd17f2c0c2f3d5698bfa5f7fb39cb906679aa10
MayuriTambe <tambemayurii190@gmail.com>
2020-02-22 10:23:12 +0530

    ADDED:Ensure the player getsto exact winning position 100

--- SnakeLadderGame.py
+++ SnakeLadderGame.py
@@ -48,3 +48,25 @@ def check_win(player_name, position):
         print("Congratulations " + player_name)
 
         sys.exit(1)
+def snake_ladder(player_name, current_value, dice_value):
+    time.sleep(PAUSE)
+    old_value = current_value
+    current_value = current_value + dice_value
+
+    if current_value > MAX_VAL:
+        print("You need " + str(MAX_VAL - old_value) + " to win this game. Keep trying.")
+        return old_value
+
+    print("\n" + player_name + " moved from " + str(old_value) + " to " + str(current_value))
+    if current_value in snakes:
+        final_value = snakes.get(current_value)
+        snake_bite(current_value, final_value, player_name)
+
+    elif current_value in ladders:
+        final_value = ladders.get(current_value)
+        ladder_jump(current_value, final_value, player_name)
+
+    else:
+        final_value = current_value
+
+    return final_value
